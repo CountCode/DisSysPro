57fe303e86e5c7991b9ffa719dffe626b5dcfac6
Ilkka Lehikoinen <ilkka.lehikoinen@helsinki.fi>
2016-01-27 13:55:56 +0200

    version 0.1

--- ex1/vector.py
+++ ex1/vector.py
@@ -9,6 +9,7 @@ import sys
 import socket
 import select
 import random
+import re
 
 # Global variables
 
@@ -20,15 +21,26 @@ vector={}
 
 def client(hostTo, portTo, sendVector):
 	s = socket.socket()         # Create a socket object
-	host = socket.gethostname() # Get local machine name
-	port = 34568                # Reserve a port for your service.	
+	host = hostTo #socket.gethostname() # Get local machine name
+	port = int(portTo) # int(ports[myId]) # 34559    # Reserve a port for your service.
 
-	s.connect((host, port))
-	print s.recv(1024)
-	s.close                     # Close the socket when done
+#DEBUGGIG
+	# print "["," ".join("{0}".format(n) for n in sendVector.values()),"]"	
+	# sendData="[ "+" ".join("{0}".format(n) for n in sendVector.values())+" ]"
+	# print sendData
+	# print host, port
 
-def localEvent():
-	increased=random.randint(1,5)
+	try:
+		s.connect((host, port))
+	except socket.error, exc:
+		print "Caught exception socket.error : %s" % exc
+	else:
+		# print "send else"
+		s.send(sendData)
+		s.close                     # Close the socket when done
+
+def localEvent(numHosts):
+	increased=random.randint(1, numHosts)
 	print "l", increased
 	return increased
 
@@ -36,14 +48,20 @@ def sendMessage(sendVector):
 	nodeId = random.randint(1, index)
 	toNode = hosts[nodeId]
 	toPort = ports[nodeId]
-#Send
-# 	client(toNode, toPort, sendVector)
+
+	#Send
+	print toNode, " ", toPort
+ 	client(toNode, toPort, sendVector)
 
 	print "s", toNode, "["," ".join("{0}".format(n) for n in sendVector.values()),"]"
 
 def recvMessage(sender, recvVector, localVector):
-	
-	print "r", sender,"["," ".join("{0}".format(n) for n in recvVector),"] ["," ".join("{0}".format(n) for n in localVector),"]"
+	for i in range(1, len(localVector)+1):
+		# print localVector[i], " ", int(recvVector[i-1])
+		if localVector[i]<int(recvVector[i-1]):
+			localVector[i]=int(recvVector[i-1])
+
+	print "r", sender,"["," ".join("{0}".format(n) for n in recvVector),"] ["," ".join("{0}".format(n) for n in localVector.values()),"]"
 
 # Main program
 
@@ -72,10 +90,10 @@ for line in f:
 
 # print configurationFile
 # print myId
-vector[myId] += localEvent()
+# vector[myId] += localEvent()
 # print vector[myId]
-sendMessage(vector)
-recvMessage("ukko123", vector, vector)
+# sendMessage(vector)
+# recvMessage("ukko123", vector, vector)
 
 
 # Server socket creation
@@ -83,7 +101,8 @@ s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
 print host
 # NOTE! Should check that the local machine is same as intented host
-port = 34567                # Reserve a port for your service.
+port = int(ports[myId])                # Reserve a port for your service.
+print port
 s.bind((host, port))        # Bind to the port
 # Making socket to be non-blocking
 s.setblocking(0)
@@ -94,17 +113,22 @@ input = [s]
 
 
 # Main loop should end when vector[myId]>=100
-while True:
+while vector[myId]<100:
 	try:
 		c, addr = s.accept()     # Establish connection with client.
 	except socket.error:
 		pass
 	else:
-		print 'Got connection from', addr[0]
-		data=c.recv(100).split(" ")
-		for vectorValue in data:
-			print vectorValue
-		recvMessage(addr[0], data, vector)	
+		# print 'Got connection from', addr[0]
+		data=c.recv(100)		# buffer (100) should be scaled along with the vector (# of nodes)
+		c.close()
+		# for vectorValue in data:
+		#	print vectorValue
+		recvVector=data.replace('[ ','').replace(' ]','').split()
+		# print recvVector
+		recvMessage(addr[0], recvVector, vector) # addr[0] is host
+			
+
 
 #	print c
 #	print 'Got connection from'  #, addr
@@ -112,25 +136,8 @@ while True:
 #	c.close()                # Close the connection
 
 	if random.randint(1,2)==1:
-		vector[myId] += localEvent()
+		vector[myId] += localEvent(len(vector))
 	else:
 		vector[myId] += 1
 		sendMessage(vector)
 
-'''
-        input_ready,output_ready,errors = select.select(input, [], [])
-       
-        for sock in input_ready:
-            if sock is s:
-                client,address = sock.accept()
-                print "Accepting socket from",address[0]
-                input.append(client)
-            else:
-                data = sock.recv(1024)
-                if data:
-                    print data
-                else:
-                    sock.close()
-                    input.remove(sock)
-                    print "Dropped connection",address[0]
-'''
